a361a12182f8805c481a0572c3b0ee0ce7a43a94
Erica Girges <ericafiorenza@gmail.com>
2020-08-20 14:41:00 -0700

    email input and test completed

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -4,12 +4,21 @@ describe("User Onboarding App", () => {
         cy.url().should("includes", "localhost")
     })
 
-    //test typing in first name into input
+    //test typing in first name into input then use assertion to verify name typed matches
     it("can type something in the 'text' input", () => {
         cy.get("input[name='fname']")
         .type("Cardi")
         .should("have.value", "Cardi")
     })
+
+    //test typing in email into specified input 
+    it("can type email in the 'text' input", () => {
+        cy.get("input[name='email']")
+        .type("cardib@email.com")
+        .should("have.value", "cardib@email.com")
+    })
+
+    //
 
 
     // const textInput = () => crypto.getRandomValues("input[fname='text']")
